a6642f33a28dcc0fb1a120bfe58ffe6fb3d8f84e
David Cloutman <david.cloutman@lampfire.com>
2013-12-07 23:55:16 +0000

    Added configurations for application host and debug mode.

--- flask/BloggleConfig_example.py
+++ flask/BloggleConfig_example.py
@@ -5,6 +5,10 @@ Author: David Cloutman
 License: MIT
 """
 
+application_host = '127.0.0.1' # The I.P. address upon which the Flask application runs. 0.0.0.0 will listen on all public IP addresses.
+
+debug_mode = False # Set to true to run the Flask application in debug mode. DO NOT USE DEBUG MODE IN PRODUCTION!
+
 mysql_host = '127.0.0.1' # The domain or I.P. address that your MySQL instance is running on.
 mysql_database = '' # Give bloggle its own database to avoid conflicts with other applications.
 mysql_username = '' # Create a non-root user for Bloggle.
--- flask/bloggle.py
+++ flask/bloggle.py
@@ -8,10 +8,12 @@ from flask import Flask
 import BloggleConfig as config
 
 app = Flask(__name__)
+if config.debug_mode:
+	app.debug = True
 
 @app.route('/')
 def index():
 	return 'Welcome to Bloggle'
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+	app.run(host = config.application_host)
